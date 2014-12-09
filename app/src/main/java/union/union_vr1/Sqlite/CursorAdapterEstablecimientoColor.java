8d2e96680edac7857bc52c90957b5d082e18ed92
ccieurlish <ccieurlish@github.com>
2014-12-09 09:22:05 -0500

    Funciòn Agregar Estado del Establecimiento y Color

--- app/src/main/java/union/union_vr1/Sqlite/CursorAdapterEstablecimientoColor.java
+++ app/src/main/java/union/union_vr1/Sqlite/CursorAdapterEstablecimientoColor.java
@@ -7,6 +7,7 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
@@ -36,36 +37,46 @@ public class CursorAdapterEstablecimientoColor extends CursorAdapter{
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
 
-        String id_establecimiento = cursor.getString(cursor.getColumnIndex(DbAdaptert_Evento_Establec.EE_id_establec));
-        String nombre_establecimiento = cursor.getString(cursor.getColumnIndex(DbAdaptert_Evento_Establec.EE_nom_establec));
-        String nombre_cliente = cursor.getString(cursor.getColumnIndex(DbAdaptert_Evento_Establec.EE_nom_cliente));
-        String doc_cliente = cursor.getString(cursor.getColumnIndex(DbAdaptert_Evento_Establec.EE_doc_cliente));
-        int id_estado_atencion = Integer.parseInt(cursor.getString(cursor.getColumnIndex(DbAdaptert_Evento_Establec.EE_id_estado_atencion)));
+            String id_establecimiento = cursor.getString(cursor.getColumnIndex(DbAdaptert_Evento_Establec.EE_id_establec));
+            String nombre_establecimiento = cursor.getString(cursor.getColumnIndex(DbAdaptert_Evento_Establec.EE_nom_establec));
+            String nombre_cliente = cursor.getString(cursor.getColumnIndex(DbAdaptert_Evento_Establec.EE_nom_cliente));
+            String doc_cliente = cursor.getString(cursor.getColumnIndex(DbAdaptert_Evento_Establec.EE_doc_cliente));
+            int id_estado_atencion = Integer.parseInt(cursor.getString(cursor.getColumnIndex(DbAdaptert_Evento_Establec.EE_id_estado_atencion)));
 
 
-        String establecimiento = "Còdigo : "+id_establecimiento+
-                "\nEstablec : "+ nombre_establecimiento+
-                "\nNombre : "+ nombre_cliente +
-                "\nDocum : "+doc_cliente;
+            String establecimiento = "Còdigo : "+id_establecimiento+
+                    "\nEstablec : "+ nombre_establecimiento+
+                    "\nNombre : "+ nombre_cliente +
+                    "\nDocum : "+doc_cliente;
 
 
 
-        TextView textView =(TextView) view;
-        textView.setText(establecimiento);
-        switch (id_estado_atencion){
-            case 1:
-                textView.setBackgroundColor(Color.BLUE);
-                break;
-            case 2:
-                textView.setBackgroundColor(Color.GREEN);
-                break;
-            case 3:
-                textView.setBackgroundColor(Color.RED);
-                break;
-            case 4:
-                textView.setBackgroundColor(Color.YELLOW);
-                break;
+            TextView textView = (TextView) view;
+            textView.setSingleLine(false);
+            textView.setText(establecimiento);
 
-        }
+            WindowManager.LayoutParams layout_791 = new WindowManager.LayoutParams();
+            layout_791.width = WindowManager.LayoutParams.MATCH_PARENT;
+            layout_791.height  = WindowManager.LayoutParams.WRAP_CONTENT;
+            textView.setLayoutParams(layout_791);
+
+            //TextView textView =(TextView) view;
+
+            textView.setText(establecimiento);
+            switch (id_estado_atencion){
+                case 1:
+                    textView.setBackgroundColor(Color.BLUE);
+                    break;
+                case 2:
+                    textView.setBackgroundColor(Color.GREEN);
+                    break;
+                case 3:
+                    textView.setBackgroundColor(Color.RED);
+                    break;
+                case 4:
+                    textView.setBackgroundColor(Color.YELLOW);
+                    break;
+
+            }
     }
 }
