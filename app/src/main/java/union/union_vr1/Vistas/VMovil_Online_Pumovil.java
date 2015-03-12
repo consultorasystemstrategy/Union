88f6687f62d97c90ffa76abc42a750dd945ef4cb
ccieurlish <ccieurlish@github.com>
2015-03-12 16:02:55 -0500

    progress bar when page load.

--- app/src/main/java/union/union_vr1/Vistas/VMovil_Online_Pumovil.java
+++ app/src/main/java/union/union_vr1/Vistas/VMovil_Online_Pumovil.java
@@ -2,11 +2,10 @@ package union.union_vr1.Vistas;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.FragmentManager;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,59 +13,84 @@ import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import union.union_vr1.AsyncTask.ExportMain;
-import union.union_vr1.AsyncTask.ImportMain;
 import union.union_vr1.Login;
 import union.union_vr1.R;
-import union.union_vr1.Sqlite.DbAdapter_Histo_Venta_Detalle;
-import union.union_vr1.Sqlite.DbAdapter_Precio;
-import union.union_vr1.Sqlite.DbAdapter_Stock_Agente;
-import union.union_vr1.Utils.DialogSincronizarOffLine;
-import union.union_vr1.Utils.MyApplication;
-
 
 public class VMovil_Online_Pumovil extends Activity {
 
-    private WebView view;
-    private VMovil_Online_Pumovil mContext;
-    ProgressDialog prgDialog;
+    private static final String url = "http://192.168.0.158:8080/SysMovilProductosUnion";
+
+    private ProgressBar progressBar;
+    private WebView webView;
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.princ_web_view);
-        mContext = this;
-        new AsyncTaskCargarPagina().execute();
+        activity = this;
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                activity.setProgress(progress * 100);
+            }
+
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                //return super.onJsAlert(view, url, message, result);
+                new AlertDialog.Builder(view.getContext()).setMessage(message).setCancelable(true).show();
+                result.confirm();
+                return true;
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.INVISIBLE);
+                super.onPageFinished(view, url);
+            }
+
+        });
+
+        webView.loadUrl(url);
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack())
+        {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        dismissProgressDialog();
     }
-
-    public void showWebPU() {
-        view = (WebView) this.findViewById(R.id.webView);
-        view.getSettings().setJavaScriptEnabled(true);
-        view.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        view.loadUrl("http://192.168.0.158:8080/SysMovilProductosUnion");
-        /*Asignamos a la vista web el cliente (navegador)
-        que hemos creado como clase privada (ver más abajo
-        y que extiende del que trae Android por defecto.
-        Esta clase maneja el navegador:*/
-        view.setWebViewClient(new MiWebViewClient());
-
-        /*Asignamos a la vista web la clase MiWebViewClient
-        que hemos creado como clase privada (ver más abajo)
-        y que extiende del que trae Android por defecto.
-        Esta clase permite controlar los eventos que se producen
-        en el navegador:*/
-        view.setWebChromeClient(new MiWebCromeClient());
-    }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -82,7 +106,7 @@ public class VMovil_Online_Pumovil extends Activity {
         int id = item.getItemId();
         switch (id) {
             case R.id.buttonRedireccionarPrincipal:
-                Intent intent = new Intent(mContext, Login.class);
+                Intent intent = new Intent(activity, Login.class);
                 finish();
                 startActivity(intent);
                 break;
@@ -93,79 +117,4 @@ public class VMovil_Online_Pumovil extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    private class MiWebViewClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-    }
-
-    //Con esta clase controlamos algunos eventos javascript del navegador
-    final class MiWebCromeClient extends WebChromeClient {
-        @Override
-        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-            new AlertDialog.Builder(view.getContext()).setMessage(message).setCancelable(true).show();
-            result.confirm();
-            return true;
-        }
-    }
-
-    class AsyncTaskCargarPagina extends AsyncTask<String, String, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    view = (WebView) mContext.findViewById(R.id.webView);
-                    view.getSettings().setJavaScriptEnabled(true);
-                    view.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-                    view.loadUrl("http://192.168.0.158:8080/SysMovilProductosUnion");
-
-                    /*Asignamos a la vista web el cliente (navegador)
-                    que hemos creado como clase privada (ver más abajo
-        y que extiende del que trae Android por defecto.
-        Esta clase maneja el navegador:*/
-                    view.setWebViewClient(new MiWebViewClient());
-
-        /*Asignamos a la vista web la clase MiWebViewClient
-        que hemos creado como clase privada (ver más abajo)
-        y que extiende del que trae Android por defecto.
-        Esta clase permite controlar los eventos que se producen
-        en el navegador:*/
-                    view.setWebChromeClient(new MiWebCromeClient());
-                }
-            });
-
-            return null;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            createProgressDialog();
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            dismissProgressDialog();
-        }
-    }
-
-    public void createProgressDialog() {
-        prgDialog = new ProgressDialog(this);
-        prgDialog.setMessage("Cargando...");
-        prgDialog.show();
-
-    }
-
-    private void dismissProgressDialog() {
-        if (prgDialog != null && prgDialog.isShowing()) {
-            prgDialog.dismiss();
-        }
-    }
-
 }
